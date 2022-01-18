9b1cb6ee111fbab1d4f6e7016876cf9844d09037
Gabriel Machado <gabrielmjj5@gmail.com>
2022-01-18 17:59:26 -0300

    Hoisting

---
+++ Javascript/hoisting.js
@@ -0,0 +1,16 @@
+console.log('a =', a)
+var a = 2
+console.log('a =', a)
+
+// = jogada para cima
+
+// var a
+// console.log('a =', a)
+// a = 2
+// console.log('a =', a)
+
+
+// com let não acontece
+console.log('b =', b)
+let b = 2
+console.log('b =', b)
