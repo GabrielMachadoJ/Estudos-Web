1a51a860e13c3adcf55f10921b60524ec4fde660
Gabriel Machado <gabrielmjj5@gmail.com>
2022-02-04 21:42:28 -0300

    Arrow Function 1

---
+++ Javascript/funcao/arrowFunction1.js
@@ -0,0 +1,21 @@
+let dobro = function (a) {
+    return 2 * a
+}
+
+dobro = (a) => {
+    return 2 * a
+}
+
+dobro = a => 2 * a
+
+console.log(dobro(2))
+
+
+let ola = function () {
+    return 'Olá'
+}
+
+ola = () => 'Olá'
+
+
+console.log(ola())
