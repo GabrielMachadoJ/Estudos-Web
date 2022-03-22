b9da2fe6efe8cd35d62de2501f1bf72734e2e583
Gabriel Machado <gabrielmjj5@gmail.com>
2022-03-22 17:50:07 -0300

    Aulas 119 até 121

--- Javascript/exerciciosJavascript/ex27.js
+++ Javascript/exerciciosJavascript/ex27.js
@@ -1,24 +1,28 @@
-function comparaCrianca(altura1, taxa1, altura2, taxa2) {
+function calcularCrescimento(altura1, taxa1, altura2, taxa2) {
     if(altura1 == altura2){
-        if(taxa1 > taxa2) {
-            return 'Criança 1 passara a 2 em 1 ano'
-
-        } else if(taxa1 < taxa2){
-            return 'Criança 2 passara a 1 em 1 ano'
-        } else {
-            return 'Os dois tem o mesmo tamanho'
+        if (taxa1 > taxa2) {
+            return 'A criança 1 ultrapassará a criança 2 em 1 ano.'
+        } else if(taxa1 < taxa2) {
+            return 'A criança 2 ultrapassará a criança 1 em 1 ano.'
+        }else{
+            return 'As crianças tem igual altura e crescimento.'
         }
     } else {
-        if(altura1 > altura2) {
+        if (altura1 > altura2) {
             if(taxa1 >= taxa2){
-                return 'A criança menor vai continuar menor'
-            } else {
-                return `A criança menor ultrapassará a maior em ${calcularTempo(altura1, taxa1, altura2, taxa2)} anos`
+                return 'A criança menor não ultrapassará a maior.'
+            }else{
+               return `A criança menor ultrapassará a maior em ${calcularTempo(altura2, taxa2, altura1, taxa1)} anos` 
+            }
+        } else {
+            if(taxa2 >= taxa1){
+                return 'A criança menor não ultrapassará a maior.'
+            }else{
+               return `A criança menor ultrapassará a maior em ${calcularTempo(altura1, taxa1, altura2, taxa2)} anos` 
             }
         }
     }
 }
-
 
 function calcularTempo(alturaMenor, taxaAlturaMenor, alturaMaior, taxaAlturaMaior){
     let qtdAnos = 0
@@ -27,7 +31,8 @@ function calcularTempo(alturaMenor, taxaAlturaMenor, alturaMaior, taxaAlturaMaio
         alturaMaior += taxaAlturaMaior
         qtdAnos++
     }
+    //console.log(qtdAnos)
     return qtdAnos
 }
 
-console.log(comparaCrianca(150, 2, 130, 4));
+console.log(calcularCrescimento(150, 2, 130, 4));
